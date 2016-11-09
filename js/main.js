39095028bb60e0460f8cf48373edcc58fb8de9da
Neeraj <nkneeraj99@gmail.com>
2016-11-09 19:37:32 +0530

    added team section

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/**
+ * Created by neerajsingh on 17/10/16.
+ */
+
+
+//Floating labels//
+
+$("input[type ='text']").click(function(){
+    var $this = $(this);
+    $this.attr("placeholder", "");
+    $this.parent().find(".float-label").addClass("label-top");
+
+});
+
+//Ends here//
+
+
